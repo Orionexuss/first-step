b3e89cbbd7a8d54a1ead1a566307ff1a494070a2
orionexus <sebastianvargaslopez36@gmail.com>
2024-11-29 22:32:29 -0500

    changin change the world

--- App/main.py
+++ App/main.py
@@ -29,7 +29,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return{"message": "Hello World"}
+    return{"message": "Hello World!!!"}
 
 @app.middleware("http")
 async def log_origin(request: Request, call_next):
